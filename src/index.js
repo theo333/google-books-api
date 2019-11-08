fe27d51b22d1e7e349026cdbda1c37298ad9c7ac
Theo Manton <theo333@gmail.com>
2019-11-08 12:11:43 -0500

    refactor getBooks

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 const inquirer = require('inquirer');
 const axios = require('axios');
 
-
-const getBooks = async (search_terms) => {
+const getBooks = async searchTerms => {
   try {
-    const key = 'AIzaSyCT1qXZGb5kCBEuUJxWhS8YzL9CgwVS6Kg';
     const fields = 'items(id,volumeInfo(title,authors,publisher))';
-    const res = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${search_terms}&startIndex=0&maxResults=5&fields=${fields}`);
-    // const res = await axios.get(`https://www.googleapis.com/books/v1/volumes`);
-    // const res = await axios.get(`https://www.googleapis.com/books/v1?fields=items`);
-    console.log('results: ', res.data)
+    const key = 'AIzaSyCT1qXZGb5kCBEuUJxWhS8YzL9CgwVS6Kg';
+
+    const res = await axios.get('https://www.googleapis.com/books/v1/volumes', {
+      params: {
+        q: searchTerms,
+        startIndex: 0,
+        maxResults: 5,
+        fields,
+      },
+    });
+    console.log(res.data.items);
+    return res.data.items;
   } catch (error) {
     console.error(error);
   }
-
-}
+};
 
 const questions = [
   {
@@ -26,23 +31,21 @@ const questions = [
 ];
 
 const bookCli = () => {
-  inquirer
-    .prompt(questions)
-    .then(({ action }) => {
-      if (action === 'Search for book') {
-        console.info('Call API to: ', action)
+  inquirer.prompt(questions).then(({ action }) => {
+    if (action === 'Search for book') {
+      console.info('Call API to: ', action);
 
-        // grab data from api
-        // display data
-        // be able to select books want to add to 
-      } else {
-        // grab reading list from json file
-        // display books
-        // selecting book will show author, publisher (use expand type)
-        console.log('display book list')
-      }
-    })
-}
+      // grab data from api
+      // display data
+      // be able to select books want to add to
+    } else {
+      // grab reading list from json file
+      // display books
+      // selecting book will show author, publisher (use expand type)
+      console.log('display book list');
+    }
+  });
+};
 
 module.exports = {
   getBooks,
