22265e78ecdb78e57ae63b64eaf77ce3460f1e5f
Theo Manton <theo333@gmail.com>
2019-11-09 14:51:06 -0500

    able to loop app, exit when want to

--- src/index.js
+++ src/index.js
@@ -50,38 +50,42 @@ const bookCli = async () => {
   clearConsole();
   console.log('Welcome to BookFinder!');
   try {
-    const firstAnswer = await inquirer.prompt(firstQuestion);
-    // console.log('firstAnswer: ', firstAnswer);
+    let exitApp = false;
+    while (!exitApp) {
+      const firstAnswer = await inquirer.prompt(firstQuestion);
+      // console.log('firstAnswer: ', firstAnswer);
 
-    if (firstAnswer.searchQuery) {
-      // console.log('searchQuery: ', firstAnswer.searchQuery);
-      const bookResults = await getBooks(firstAnswer.searchQuery);
-      // console.log('bookResults: ', bookResults);
-      // console.log('bookResults: ', JSON.stringify(bookResults));
+      if (firstAnswer.searchQuery) {
+        // console.log('searchQuery: ', firstAnswer.searchQuery);
+        const bookResults = await getBooks(firstAnswer.searchQuery);
+        // console.log('bookResults: ', bookResults);
+        // console.log('bookResults: ', JSON.stringify(bookResults));
 
-      // add to list prompt
-      const addToListAnswer = await inquirer.prompt({
-        type: 'checkbox',
-        name: 'addToList',
-        message:
-          'Here are the results of your query. Select all you want to add to your Reading List',
-        async choices() {
-          return formatBookResults(bookResults);
-        },
-      });
+        // add to list prompt
+        const addToListAnswer = await inquirer.prompt({
+          type: 'checkbox',
+          name: 'addToList',
+          message:
+            'Here are the results of your query. Select all you want to add to your Reading List',
+          async choices() {
+            return formatBookResults(bookResults);
+          },
+        });
 
-      if (addToListAnswer.addToList) {
-        addToReadingList(bookResults, addToListAnswer.addToList);
+        if (addToListAnswer.addToList) {
+          addToReadingList(bookResults, addToListAnswer.addToList);
+        }
       }
-    }
 
-    if (firstAnswer.action === 'list') {
-      // console.log('getReadingList: ', getReadingList());
-      getReadingList();
-    }
+      if (firstAnswer.action === 'list') {
+        // console.log('getReadingList: ', getReadingList());
+        getReadingList();
+      }
 
-    if (firstAnswer.action === 'exit') {
-      console.log('exit');
+      if (firstAnswer.action === 'exit') {
+        console.log('Have a great day!\n');
+        exitApp = true;
+      }
     }
   } catch (error) {
     throw new Error(error);
