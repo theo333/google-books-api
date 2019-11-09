a1cffe8e3f8153c5837b2eb54209797f2ee1b9d6
Theo Manton <theo333@gmail.com>
2019-11-08 20:36:07 -0500

    able to get and add to reading list

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const axios = require('axios');
 const readline = require('readline');
+const { existsSync, readFileSync, writeFileSync } = require('fs');
 
 // TODO move all helper functions to utils.js
 const clearConsole = () => {
@@ -17,7 +18,7 @@ const getBooks = async searchQuery => {
     // TODO in production app would move to .env file and grab using process.env
     const key = 'AIzaSyCT1qXZGb5kCBEuUJxWhS8YzL9CgwVS6Kg';
 
-    const res = await axios.get('https://www.googleapis.com/books/v1/volumes', {
+    const response = await axios.get('https://www.googleapis.com/books/v1/volumes', {
       params: {
         q: searchQuery,
         startIndex: 0,
@@ -25,7 +26,8 @@ const getBooks = async searchQuery => {
         fields,
       },
     });
-    return res.data.items;
+    const books = response.data.items.map(x => x.volumeInfo);
+    return books;
   } catch (error) {
     console.error(error);
   }
@@ -33,7 +35,7 @@ const getBooks = async searchQuery => {
 
 const formatBookResults = results => {
   const formatted = results.map(x => {
-    const { title, authors, publisher } = x.volumeInfo;
+    const { title, authors, publisher } = x;
     return {
       name: `${title} by ${authors.join(', ')}, published by ${publisher}`,
       value: title,
@@ -44,7 +46,7 @@ const formatBookResults = results => {
   return formatted;
 };
 
-const questions = [
+const firstQuestion = [
   {
     type: 'list',
     name: 'action',
@@ -59,55 +61,113 @@ const questions = [
       return answers.action === 'Search for book';
     },
   },
-  {
-    type: 'checkbox',
-    name: 'results',
-    message: 'Here are the results of your query. Select all you want to add to your Reading List',
-    async choices(answers) {
-      const results = await getBooks(answers.searchQuery);
-      return formatBookResults(results);
-    },
-    when(answers) {
-      return answers.searchQuery;
-    },
-  },
+  // {
+  //   type: 'checkbox',
+  //   name: 'results',
+  //   message: 'Here are the results of your query. Select all you want to add to your Reading List',
+  //   async choices(answers) {
+  //     const results = await getBooks(answers.searchQuery);
+  //     return formatBookResults(results);
+  //   },
+  //   when(answers) {
+  //     return answers.searchQuery;
+  //   },
+  // },
 ];
+
+// const questions2 = [
+//   {
+//     type: 'checkbox',
+//     name: 'results',
+//     message: 'Here are the results of your query. Select all you want to add to your Reading List',
+//     async choices(bookResults) {
+//       // const results = await getBooks(answers.searchQuery);
+//       return formatBookResults(bookResults);
+//     },
+//   },
+// ];
 
 const bookCli = async () => {
   clearConsole();
   console.log('Welcome to BookFinder!');
-  // try {
-  //   // prompt:  Search | List
-  //   // answer: search
-  //   // prompt (when): input search query
-  //   // answer: call getBooks(searchQuery)
-  //   // prompt (when): display results
-  //   const answers1 = await inquirer.prompt(questions);
-  //   console.log(answers1);
+  try {
+    // prompt:  Search | List
+    // answer: search
+    // prompt (when): input search query
+    // answer: call getBooks(searchQuery)
+    // prompt (when): display results
+    const firstAnswer = await inquirer.prompt(firstQuestion);
+    // console.log('firstAnswer: ', firstAnswer);
 
-  //   if (answers1.searchQuery) {
-  //     console.log('searchQuery: ', answers1.searchQuery);
-  //   }
-  // } catch (error) {
-  //   throw new Error(error);
-  // }
+    if (firstAnswer.searchQuery) {
+      console.log('searchQuery: ', firstAnswer.searchQuery);
+      const bookResults = await getBooks(firstAnswer.searchQuery);
+      console.log('bookResults: ', bookResults);
+      console.log('bookResults: ', JSON.stringify(bookResults));
 
-  inquirer.prompt(questions).then(({ action, searchQuery, results }) => {
-    if (action === 'Search for book') {
-      console.info('Call API to: ', action);
+      // add to list prompt
+      const addToListAnswer = await inquirer.prompt({
+        type: 'checkbox',
+        name: 'addToList',
+        message: 'Here are the results of your query. Select all you want to add to your Reading List',
+        async choices() {
+          return formatBookResults(bookResults);
+        },
+      });
 
-      // grab data from api
-      // display data
-      // be able to select books want to add to
-    } else {
-      // grab reading list from json file
-      // display books
-      // selecting book will show author, publisher (use expand type)
-      console.log('display book list');
+      if (addToListAnswer.addToList) {
+
+        const addToReadingList = (bookResults, itemsToAddTitle) => {
+          console.log('itemsToAddTitle: ', itemsToAddTitle);
+
+          // grab json file
+          const fileUrl = `${process.cwd()}/src/list.json`;
+          let oldList = [];
+          if (existsSync(fileUrl)) {
+            const rawData = readFileSync(fileUrl);
+            console.log('rawData: ', rawData.length);
+            oldList = rawData.length ? JSON.parse(rawData) : [];
+          }
+          console.log('oldList: ', oldList);
+
+          // filter bookResults to only include addToList items
+          const itemsToAdd = bookResults.filter(x => itemsToAddTitle.includes(x.title));
+          // console.log('itemsToAdd: ', itemsToAdd);
+
+          // oldList.concat(newBooksToAdd_array)
+          // TODO check to make sure item is not already in oldList before add so that do not get duplicates
+          const newList = oldList.concat(itemsToAdd);
+          console.log('newList: ', newList)
+
+          // save json file
+          const data = JSON.stringify(newList);
+          writeFileSync(fileUrl, data);
+
+        };
+
+        addToReadingList(bookResults, addToListAnswer.addToList);
+      }
     }
-    // if (searchQuery) console.log('searchQuery: ', getBooks(searchQuery));
-    if (results) console.log('results: ', results);
-  });
+  } catch (error) {
+    throw new Error(error);
+  }
+
+  // inquirer.prompt(questions).then(({ action, searchQuery, results }) => {
+  //   if (action === 'Search for book') {
+  //     console.info('Call API to: ', action);
+
+  //     // grab data from api
+  //     // display data
+  //     // be able to select books want to add to
+  //   } else {
+  //     // grab reading list from json file
+  //     // display books
+  //     // selecting book will show author, publisher (use expand type)
+  //     console.log('display book list');
+  //   }
+  //   // if (searchQuery) console.log('searchQuery: ', getBooks(searchQuery));
+  //   if (results) console.log('results: ', results);
+  // });
 };
 
 module.exports = {
